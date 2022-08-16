cd66cfc15586005b9eae9e1423d2dc81a5e40c02
rhksrb99 <rhksrb99@gmail.com>
2022-08-16 09:44:03 +0900

    Initial Commit

---
+++ SeSAC2Week7Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC2Week7Diary
+//
+//  Created by 박관규 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
